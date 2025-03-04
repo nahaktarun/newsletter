c5ec4494b4261cd5fee6c3892219a53fc0254061
Tarun Nahak <81190291+nahaktarun@users.noreply.github.com>
2025-03-04 23:20:39 +0530

    Added the server start

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,8 +4,44 @@ package main
 
 import (
 	"fmt"
+	"go.uber.org/zap"
+	"os"
 )
 
 func main() {
-	fmt.Println("🤓")
+	os.Exit(start())
+}
+
+func start() int{
+	logEnv := getStringOrDefault("LOG_ENV", "development")
+	log, err := createLogger(logEnv)
+	if err != nil{
+		fmt.Println("Error setting up the logger", err)
+		return 1
+	}
+
+	defer func() {
+		_ = log.Sync()
+	}()
+	return 0
+}
+
+
+func createLogger(env string) (*zap.Logger, error){
+	switch env {
+	case  "production":
+		return zap.NewProduction()
+	case "development":
+		return zap.NewDevelopment()
+	default:
+		return zap.NewNop(), nil
+	}
+}
+
+func getStringOrDefault(name, defaultV string) string{
+	v, ok := os.LookupEnv(name)
+	if !ok{
+		return defaultV
+	}
+	return v
 }
